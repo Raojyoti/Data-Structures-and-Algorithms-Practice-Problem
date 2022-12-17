69b9b5975ac70c9cca8985207b051c54ad767df4
Raojyoti <117572975+RaoJyoti123@users.noreply.github.com>
2022-12-17 18:55:10 +0530

    Prime numbers that are Palindrome

---
+++ Prime Numb Anagram and Palindrome/DisplayPrimePalindrome.cs
@@ -0,0 +1,52 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace Prime_Numb_Anagram_and_Palindrome
+{
+    public class DisplayPrimePalindrome
+    {
+        public void DisplayPrimePalindromeNumber()
+        {
+
+            int count = 0;
+
+            for (int i = 2; i <= 1000; i++)
+            {
+
+                int num = i, revNum = 0;
+                while (num != 0)
+                {
+                    int digit = num % 10;
+                    num /= 10;
+                    revNum = revNum * 10 + digit;
+                }
+
+
+                if (revNum == i)
+                {
+                    bool isPrime = true;
+                    for (int j = 2; j <= i / 2; j++)
+                    {
+
+                        if (i % j == 0)
+                        {
+                            isPrime = false;
+                            break;
+                        }
+
+                    }
+                    if (isPrime)
+                    {
+                        Console.WriteLine(i + " ");
+                        count++;
+                        if (count == 10)
+                        {
+                            count = 0;
+                        }
+                    }
+                }
+            }
+        }
+    }
+}
---
+++ Prime Numb Anagram and Palindrome/Program.cs
@@ -0,0 +1,14 @@
+﻿using System;
+
+namespace Prime_Numb_Anagram_and_Palindrome
+{
+    public class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Prime numbers that are Palindrome: ");
+            DisplayPrimePalindrome obj=new DisplayPrimePalindrome();
+            obj.DisplayPrimePalindromeNumber();
+        }
+    }
+}
